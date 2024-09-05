8ed9564d7a87d8485e30cd53927baba9f3103aee
rajatgupta2004 <rajatgupta562004@gmail.com>
2024-09-05 16:36:26 +0530

    hello

--- backend/server.js
+++ backend/server.js
@@ -10,6 +10,9 @@ app.use(bodyParser.json());
 const PORT=process.env.PORT||3000;
 app.use('/user',userroute)
 app.use('/medicine',medicineroute)
+app.use('/',(req,res)=>{
+    res.send("HELLO");
+})
 
 app.listen(3000,()=>{
     console.log('connected to server 3000');
