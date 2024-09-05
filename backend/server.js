15a47b75a372e7e015aeb7f400636352a055eac1
rajatgupta2004 <rajatgupta562004@gmail.com>
2024-09-05 12:42:47 +0530

    last try

--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,11 @@
 const express=require('express');
-const cors = require('cors');
+const cors = require('cors')
 const app = express();
 const db=require('./db');
 const userroute=require('./routes/userRoutes.js');
 const medicineroute=require('./routes/medicineRoutes.js');
 const bodyParser=require('body-parser');
-app.all("/*", function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Cache-Control, Pragma, Origin, Authorization, Content-Type, X-Requested-With");
-    res.header("Access-Control-Allow-Methods", "GET, PUT, POST");
-    return next();
-  });
-
+app.use(cors());
 app.use(bodyParser.json());
 const PORT=process.env.PORT||3000;
 app.use('/user',userroute)
