cf1cc31c13a6beccf60aa8d671dde86363f71a50
rajatgupta2004 <rajatgupta562004@gmail.com>
2024-09-05 12:17:39 +0530

    new cors added

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,12 @@ const db=require('./db');
 const userroute=require('./routes/userRoutes.js');
 const medicineroute=require('./routes/medicineRoutes.js');
 const bodyParser=require('body-parser');
-app.use(cors());
+app.all("/api/*", function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Cache-Control, Pragma, Origin, Authorization, Content-Type, X-Requested-With");
+    res.header("Access-Control-Allow-Methods", "GET, PUT, POST");
+    return next();
+  });
 
 app.use(bodyParser.json());
 const PORT=process.env.PORT||3000;
